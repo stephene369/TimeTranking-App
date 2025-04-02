f853b257b2fbd1a646b6a3d2fe72ff20b8a629ec
Stephene WANTCHEKON <stephenew36@gmail.com>
2025-04-02 17:44:57 +0100

    Version-02/04/2025/17-44-57

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ConfigProvider } from 'antd'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
