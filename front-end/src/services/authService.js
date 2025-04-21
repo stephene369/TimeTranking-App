9c478f7241520fa9269932c6bdc3ced4ba80e332
Stephene WANTCHEKON <stephenew36@gmail.com>
2025-04-21 20:34:13 +0100

    Version-21/04/2025/20-34-13

--- front-end/src/services/authService.js
+++ front-end/src/services/authService.js
@@ -1,5 +1,5 @@
 // Constants
-const API_URL = 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 const TOKEN_KEY = 'auth_token';
 const REFRESH_TOKEN_KEY = 'refresh_token';
 const USER_KEY = 'user_info';
