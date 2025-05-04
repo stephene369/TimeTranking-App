203dad113e249c3e8104023940e85d832c515c8a
Stephene WANTCHEKON <stephenew36@gmail.com>
2025-05-04 17:13:08 +0100

    Version-04/05/2025/17-13-08

--- backend/back_end/settings.py
+++ backend/back_end/settings.py
@@ -282,6 +282,7 @@ EMAIL_PORT = 587  # 465 si vous utilisez SSL
 EMAIL_USE_TLS = True  # False si vous utilisez SSL
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+# EMAIL_HOST_PASSWORD =  "oeks anta xebw hfiw"
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
 
